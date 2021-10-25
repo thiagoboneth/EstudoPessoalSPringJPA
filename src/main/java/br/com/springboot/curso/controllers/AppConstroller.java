284bc51c7304b7c9eb7f2ea9840fd8db0ab9adab
Thiago <t_boneth@hotmail.com>
2021-10-25 17:00:04 -0300

    Implementando buscar por nome

--- src/main/java/br/com/springboot/curso/controllers/AppConstroller.java
+++ src/main/java/br/com/springboot/curso/controllers/AppConstroller.java
@@ -57,7 +57,7 @@ public class AppConstroller {
     @GetMapping("/buscarPorNome")
     @ResponseBody
     public ResponseEntity<List<Usuario>> buscarPorNome (@RequestParam(name = "name") String name){
-        List<Usuario> usuario = usuarioRepository.buscarPorNome(name);
+        List<Usuario> usuario = usuarioRepository.buscarPorNome(name.trim().toUpperCase());
         return new ResponseEntity<List<Usuario>>(usuario, HttpStatus.OK);
     }
 }
